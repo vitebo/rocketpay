d2fa2c1d2572f9bb35aa2b0b3db0427f33630cf7
vitebo <vitebo@hotmail.com>
2021-02-25 23:02:02 -0300

    feat(rocketpay): create transactions

--- lib/rocketpay/accounts/operation.ex
+++ lib/rocketpay/accounts/operation.ex
@@ -4,13 +4,20 @@ defmodule Rocketpay.Accounts.Operation do
   alias Rocketpay.Account
 
   def call(%{"id" => id, "value" => value}, operation) do
+    operation_name = account_operation_name(operation)
     Multi.new()
-    |> Multi.run(:account, fn repo, _changes ->
+    |> Multi.run(operation_name, fn repo, _changes ->
       get_account(repo, id)
     end)
-    |> Multi.run(:update_balance, fn repo, %{account: account} ->
+    |> Multi.run(operation, fn repo, changes ->
+      account = Map.get(changes, operation_name)
       update_balance(repo, account, value, operation)
     end)
+  end
+
+  defp account_operation_name(operation) do
+    "account_#{Atom.to_string(operation)}"
+    |> String.to_atom()
   end
 
   defp get_account(repo, id) do
